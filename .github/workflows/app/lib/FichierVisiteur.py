7295ab94079bf68266711d377dbb1f704d8f686d
galaono <alexis.pinchon268@gmail.com>
2024-12-18 09:56:00 +0100

    Change

--- .github/workflows/app/lib/FichierVisiteur.py
+++ .github/workflows/app/lib/FichierVisiteur.py
@@ -1,18 +1,22 @@
-import json
+from Validateur import Validateur
 
 class FichierVisiteur:
-    def __init__(self, chemin_fichier: str):
+
+    def __init__(self, chemin_fichier):
         self.chemin_fichier = chemin_fichier
 
-    def ajout(self, visiteur: dict):
-        visiteurs = self.tout_lire()
-        visiteurs.append(visiteur)
-        with open(self.chemin_fichier, 'w') as fichier:
-            json.dump(visiteurs, fichier)
+    def ajout(self, nom, prenom):
+
+        if Validateur.valider_nom_prenom(nom) and Validateur.valider_nom_prenom(prenom):
+            with open(self.chemin_fichier, 'a', encoding='utf-8') as fichier:
+                fichier.write(f"{nom},{prenom}\n")
+            return True
+        return False
 
     def tout_lire(self):
+
         try:
-            with open(self.chemin_fichier, 'r') as fichier:
-                return json.load(fichier)
+            with open(self.chemin_fichier, 'r', encoding='utf-8') as fichier:
+                return [ligne.strip() for ligne in fichier.readlines()]
         except FileNotFoundError:
             return []
--- .github/workflows/app/lib/Validateur.py
+++ .github/workflows/app/lib/Validateur.py
@@ -2,14 +2,15 @@ import re
 
 class Validateur:
     @staticmethod
-    def valider_nom_prenom(valeur: str) -> bool:
+    def valider_nom_prenom(valeur):
 
         if not (2 <= len(valeur) <= 60):
             return False
-        if valeur.startswith(("-", " ")) or valeur.endswith(("-", " ")):
+
+        if not re.match(r"^[a-zA-Z]([- a-zA-Z]{0,58}[a-zA-Z])?$", valeur):
             return False
+
         if valeur.count('-') > 5 or valeur.count(' ') > 5:
             return False
-        if not re.match(r'^[a-zA-Z\s\-]+$', valeur):
-            return False
+
         return True
