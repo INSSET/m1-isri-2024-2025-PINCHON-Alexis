1a1fb064494e9946497c121ab3be071b444f49a1
galaono <alexis.pinchon268@gmail.com>
2024-12-18 12:58:19 +0100

    l

--- app/lib/FichierVisiteur.py
+++ app/lib/FichierVisiteur.py
@@ -2,12 +2,14 @@ from app.lib.Validateur import Validateur
 
 class FichierVisiteur:
 
+
     def __init__(self, chemin_fichier):
         self.chemin_fichier = chemin_fichier
 
     def ajout(self, nom, prenom):
 
-        if Validateur.valider_nom_prenom(nom) and Validateur.valider_nom_prenom(prenom):
+        if (Validateur.valider_nom_prenom(nom)
+                and Validateur.valider_nom_prenom(prenom)):
             with open(self.chemin_fichier, 'a', encoding='utf-8') as fichier:
                 fichier.write(f"{nom},{prenom}\n")
             return True
